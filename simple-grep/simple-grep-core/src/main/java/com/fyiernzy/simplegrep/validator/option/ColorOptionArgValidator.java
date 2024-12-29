649774d2dcfcec202b741d23fb7289293d1851fb
zhiyang.ng <zhiyang.ng@antgroup.com>
2024-12-29 10:34:28 +0800

    feat: last commit from Ant

--- simple-grep/simple-grep-core/src/main/java/com/fyiernzy/simplegrep/validator/option/ColorOptionArgValidator.java
+++ simple-grep/simple-grep-core/src/main/java/com/fyiernzy/simplegrep/validator/option/ColorOptionArgValidator.java
@@ -4,6 +4,11 @@ import org.apache.commons.lang3.Validate;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -23,5 +28,28 @@ public class ColorOptionArgValidator implements OptionArgValidator {
         Validate.notBlank(possibleValue, "Color value cannot be blank");
         Validate.isTrue(SUPPORTED_COLOR.contains(possibleValue));
     }
+
+    public static void main(String[] args) {
+        // LocalDate instances
+        LocalDate newDate = LocalDate.of(2024, 12, 13);
+        LocalDate oldDate = null;
+
+        // Convert LocalDate to Date
+        Date newDateConverted = convertLocalDateToDate(newDate);
+        Date oldDateConverted = oldDate != null ? convertLocalDateToDate(oldDate) : null;
+
+        System.out.println("Converted newDate: " + newDateConverted);
+        System.out.println("Converted oldDate: " + oldDateConverted);
     
+        if(newDateConverted.before(oldDateConverted)) {
+            System.out.println("newDate is before oldDate");
+        }
+    }
+
+    public static Date convertLocalDateToDate(LocalDate localDate) {
+        // Convert LocalDate to LocalDateTime (midnight of the given date)
+        LocalDateTime localDateTime = localDate.atStartOfDay();
+        // Convert LocalDateTime to Date
+        return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+    }
 }
